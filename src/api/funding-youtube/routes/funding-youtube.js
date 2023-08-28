34cd1dc576fb70c8bcfb794bb04a49e9690a9845
Kalinichenko <kalinamono@gmail.com>
2023-08-28 15:18:21 +0300

    add funding

---
+++ src/api/funding-youtube/routes/funding-youtube.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * funding-youtube router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::funding-youtube.funding-youtube');
